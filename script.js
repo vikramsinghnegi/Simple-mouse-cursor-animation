63c89ff18eb492210e533a9737bcc53a1d70c8a1
vikram singh <100604328+vikramsinghnegi@users.noreply.github.com>
2024-09-04 17:20:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var main = document.querySelector("#main")
+var cursor = document.querySelector("#cursor")
+ var imageDiv= document.querySelector("#image")
+
+ main.addEventListener("mousemove",function(dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y,
+        duration:0.6,
+       
+        
+         })
+
+    })
+   
+    imageDiv.addEventListener("mouseenter",function(){
+        cursor.innerHTML="View More"
+       gsap.to(cursor,{
+        scale:4,
+        backgroudColor:"#ffffff8a"
+       })
+    })
+
+    imageDiv.addEventListener("mouseleave",function(){
+        cursor.innerHTML=""
+        gsap.to(cursor,{
+         scale:1,
+         backgroudColor:"#fff"
+        })
+     })
